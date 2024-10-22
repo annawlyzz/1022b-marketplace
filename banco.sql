b178a5194a1453da19b28b1efb26cdf233112f93
anna <ana.pego@estudante.ifms.edu.br>
2024-10-22 07:56:21 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS produtos(
     imagem VARCHAR(300)
 );
 INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+
+USE defaultdb;
+CREATE TABLE IF NOT EXISTS usuarios(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(50),
+    created_at TIMESTAMP,
+    update_at TIMESTAMP
+);
+INSERT INTO usuarios VALUES (1,'Ana','anaeleuterio@gmail.com', '2024-10-22 07:28:20', '2024-10-22 07:29:00 ');
