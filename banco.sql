21b98249b15a8089fb29a22a734041706db91819
anna <ana.pego@estudante.ifms.edu.br>
2024-10-22 07:43:32 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'https://m.media-amazon.com/images/I/41TqO2RM7QL._AC_SX679_.jpg');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
