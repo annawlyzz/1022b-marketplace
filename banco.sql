d3b44dd7f1eec6822d1e0af254f966f0edac6161
anna <ana.pego@estudante.ifms.edu.br>
2025-01-22 08:39:32 -0400

    Atualização 22/01

--- banco.sql
+++ banco.sql
@@ -1,4 +1,6 @@
-USE defaultdb;
+drop database banco1022b;
+create database banco1022b;
+USE banco1022b;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -6,14 +8,4 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
-
-USE defaultdb;
-CREATE TABLE IF NOT EXISTS usuarios(
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50),
-    email VARCHAR(50),
-    created_at TIMESTAMP,
-    update_at TIMESTAMP
-);
-INSERT INTO usuarios VALUES (1,'Ana','anaeleuterio@gmail.com', '2024-10-22 07:28:20', '2024-10-22 07:29:00 ');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'https://m.media-amazon.com/images/I/515ahREcK+L._AC_SX679_.jpg');
